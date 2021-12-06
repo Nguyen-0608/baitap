e47b5e1dce4656912b5410b54a1a202669425943
Nguyen-0608 <95267063+Nguyen-0608@users.noreply.github.com>
2021-12-06 21:13:50 +0700

    còn chưa tính được đtb mỗi bạn

--- Chọn SV nhận học bổng.py
+++ Chọn SV nhận học bổng.py
@@ -1,20 +1,8 @@
 # Nhập số lượng SV
 n=int(input("Nhập vào số lượng sinh viên:"))
 #Danh sách các môn học
-dscacmon=["TTHCM","GTN","VHH","QHQT","TDLT","TCC","LLNN","KTVM","TA","ĐRL"]
-sotinchi=[  2,     2,     2,    2,     3,     3,    3,     3,    5,    0]
-#Hàm nhập điểm cho các bạn sv
-def nhapdiem(dsmon):
-    ds=[]
-    for i in range(1, n + 1):
-       print("Nhập điểm của bạn thứ", i)
-       for i in range(len(dsmon)):
-          print("Nhập môn" +dsmon[i])
-          diem=float(input())
-          ds.append(diem)
-    dtb=trungbinh(diem)
-    return(ds)
-ds=nhapdiem(dscacmon)
+dscacmon=[" TTHCM"," GTN"," VHH"," QHQT"," TDLT"," TCC"," LLNN"," KTVM"," TA"," ĐRL"]
+sotinchi=[   2,      2,      2,     2,      3,      3,     3,      3,       5,   0]
 
 #Tổng điểm các môn
 def tongdiem(ds,tinchi):
@@ -28,13 +16,31 @@ def tongdiem(ds,tinchi):
     d8 = ds[7] * tinchi[7]
     d9 = ds[8] * tinchi[8]
     d10 = ds[9]* tinchi[9]
-    tong=d1+d2+d3+d4+d5+d6+d7+d8+d9+d10
+    tong=d1+d2+d3+d4+d5+d6+d7+d8+d9+(d10*0.2)
     return(tong)
-diem=float(tongdiem(ds,sotinchi))
-#Điểm trung bình
-def trungbinh(tong):
-    dtb=tong/25
-    return dtb
 
+#Điểm trung bình
+def trungbinh(tong,sotinchi):
+     tc=0
+     for i in range(len(sotinchi)):
+         tc+=sotinchi[i]
+     dtb=tong/tc
+     return dtb
+
+#Hàm nhập điểm cho các bạn sv
+def nhapdiem(dsmon):
+    ds=[]
+    for i in range(1, n + 1):
+       print("Nhập điểm của bạn thứ", i)
+       for i in range(len(dsmon)):
+          print("Nhập môn" + dsmon[i])
+          diem=float(input())
+          ds.append(diem)
+    return(ds)
+ds = nhapdiem(dscacmon)
+#Output
+diem=tongdiem(ds,sotinchi)
+tb=trungbinh(diem,sotinchi)
+print("Điểm trung bình mỗi bạn là",tb)
 
 
